9235c21163cb525e615aef20792372340a64f16b
Tommy He <tommy@tommyhe.com>
2023-01-05 02:31:47 -0500

    cleanup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,23 +1,23 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--single-branch",
-        "https://github.com/folke/lazy.nvim.git",
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--single-branch",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 end
 vim.opt.runtimepath:prepend(lazypath)
 
 require("keymaps")
 require("options")
 require("lazy").setup("plugins", {
-    install = { colorscheme = { "dracula" } },
-    performance = {
-        cache = {
-            enabled = true,
-        },
-    },
+	install = { colorscheme = { "dracula" } },
+	performance = {
+		cache = {
+			enabled = true,
+		},
+	},
 })
