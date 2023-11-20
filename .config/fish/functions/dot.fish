4fffb993b431af4e0ea43eaaf5d5eec022679fca
Tommy He <tommyhe6@gmail.com>
2023-11-19 23:47:49 -0500

    Clean up

--- .config/fish/functions/dot.fish
+++
@@ -1,3 +0,0 @@
-function dot --wraps='git --git-dir=/Users/tommyhe/.dotfiles/ --work-tree=/Users/tommyhe' --description 'alias dot=git --git-dir=/Users/tommyhe/.dotfiles/ --work-tree=/Users/tommyhe'
-  git --git-dir=/Users/tommyhe/.dotfiles/ --work-tree=/Users/tommyhe $argv; 
-end
